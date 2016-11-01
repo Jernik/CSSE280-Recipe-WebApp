329d45075d7c203f3db8b7ce2f2e6f7810c20b2f
Tim Smith <timothys740@gmail.com>
2016-10-31 21:15:16 -0400

    Finished up JS framework for profile page

--- Profile.js
+++ Profile.js
@@ -1,6 +1,15 @@
 var profileImg = "images/profile.png";
+var email = "abcd@example.com";
+var recipeNumber = 5;
 //function that places profile picture
 function placeImage(){
-    $('.roundbox').append('<img src="'+profileImg+'" width=200px height=200px />');
+    $('.roundbox').append('<img id ="profileImg" src="'+profileImg+'" width=200px height=200px />');
+}
+// function that places profile info
+function placeInfo(){
+    $('.roundbox').append('<p class="text">Email: '+email+'</p>');
+    $('.roundbox').append('<a href="URL ADDED HERE" class="text">Recipes: '+recipeNumber+'</a>');
+    $('.roundbox').append('<button id="editButton" > Edit Profile </button>')
 }
 placeImage();
+placeInfo();
