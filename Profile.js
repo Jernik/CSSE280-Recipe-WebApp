ecaf4a946204d9441773f395a9689afcef662611
Tim Smith <timothys740@gmail.com>
2016-10-31 20:14:04 -0400

    Setup inclusion of javascript and added profile picture

---
+++ Profile.js
@@ -0,0 +1,6 @@
+var profileImg = "images/profile.png";
+//function that places profile picture
+function placeImage(){
+    $('.roundbox').append('<img src="'+profileImg+'" width=200px height=200px />');
+}
+placeImage();
