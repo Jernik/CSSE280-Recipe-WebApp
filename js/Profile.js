345bc1567069f72997c993012aedfdb306e90762
Tim Smith <timothys740@gmail.com>
2016-11-01 22:25:50 -0400

    Added a add friend button to profile page

--- js/Profile.js
+++ js/Profile.js
@@ -1,6 +1,7 @@
 var profileImg = "images/profile.png";
 var email = "abcd@example.com";
 var recipeNumber = 5;
+var profileId = 0;
 //function that places profile picture
 function placeImage(){
     $('.roundbox').append('<img id ="profileImg" src="'+profileImg+'" width=200px height=200px />');
@@ -9,7 +10,13 @@ function placeImage(){
 function placeInfo(){
     $('.roundbox').append('<p class="text">Email: '+email+'</p>');
     $('.roundbox').append('<a href="URL ADDED HERE" class="text">Recipes: '+recipeNumber+'</a>');
-    $('.roundbox').append('<button id="editButton" > Edit Profile </button>')
+    //If profile is logged in profile then display edit profile else show add friend
+    if (profileId === 1){
+        $('.roundbox').append('<button class="Button" > Edit Profile </button>');
+    } else{
+        // check if friend and if so show remove friend
+        $('.roundbox').append('<button class="Button" > Add Friend </button>');
+    }
 }
 placeImage();
 placeInfo();
