a39da0f330eb1618cdea4c732b1677338f3e0507
Tim Smith <timothys740@gmail.com>
2016-11-01 23:32:02 -0400

    started support for adding a friend on frontend

--- js/Profile.js
+++ js/Profile.js
@@ -2,6 +2,8 @@ var profileImg = "images/profile.png";
 var email = "abcd@example.com";
 var recipeNumber = 5;
 var profileId = 0;
+// TODO To be changed later
+var apiUrl = "https://csse280-contact-back-smithtl.herokuapp.com/contacts";
 //function that places profile picture
 function placeImage(){
     $('.roundbox').append('<img id ="profileImg" src="'+profileImg+'" width=200px height=200px />');
@@ -10,13 +12,30 @@ function placeImage(){
 function placeInfo(){
     $('.roundbox').append('<p class="text">Email: '+email+'</p>');
     $('.roundbox').append('<a href="URL ADDED HERE" class="text">Recipes: '+recipeNumber+'</a>');
-    //If profile is logged in profile then display edit profile else show add friend
+    // TODO If profile is logged in profile then display edit profile else show add friend
     if (profileId === 1){
         $('.roundbox').append('<button class="Button" > Edit Profile </button>');
     } else{
-        // check if friend and if so show remove friend
-        $('.roundbox').append('<button class="Button" > Add Friend </button>');
+        // TODO check if friend and if so show remove friend
+        $('.roundbox').append('<button class="Button" onClick=addFriend()> Add Friend </button>');
     }
 }
 placeImage();
 placeInfo();
+
+// adding friend to your friends list
+function addFriend() {
+    // TODO get friend list and add friend to list
+    $.ajax({
+        url: apiUrl+profile._id,
+        type: 'PUT',
+        dataType: 'JSON',
+        data: profile,
+        success: function (data) {
+            console.log("Updated contact");
+        },
+        error: function (request, status, error) {
+            console.log(error, status, request);
+        }
+    });
+}
